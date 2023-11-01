6227b01ae280218ebf4864fa8fbdac76a0f7adf4
t.xavier.ferreira <t.xavier.ferreira@avanade.com>
2023-11-01 15:50:53 -0300

    Removido classe WeatherForecast, alterado nome da classe WeatherForescastController e alterado implementação de CalculatorController

---
+++ RestWithAspNet/RestWithAspNet/Controllers/CalculatorController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithAspNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        private int ConvertToDecimal(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsNumeric(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
