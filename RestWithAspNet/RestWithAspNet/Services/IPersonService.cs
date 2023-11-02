31e0271cbdaabf7ff2d250dd9cf37674e9614c84
t.xavier.ferreira <t.xavier.ferreira@avanade.com>
2023-11-02 17:22:32 -0300

    Criado interface IPersonService dentro das pastas Services e Implementations

---
+++ RestWithAspNet/RestWithAspNet/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestWithAspNet.Controllers.Model;
+using System.Collections.Generic;
+
+namespace RestWithAspNet.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindbyId(long id);
+        List<Person> FindAll();
+        void Delete(long id);
+    }
+}
