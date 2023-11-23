54e567879b21c8106a78440a198a3ba95988c586
thiago_osim <thiago_osim@hotmail.com>
2023-11-22 22:35:29 -0300

    Ajustado a classe Person e criado a classe PersonServiceImplementation na pasta Implementations

---
+++ RestWithAspNet/RestWithAspNet/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System.Threading;
+using RestWithAspNet.Model;
+
+namespace RestWithAspNet.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        public Person FindbyId(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Goiaba",
+                LastName = "Goiabada",
+                Address = "São paulo - São paulo - Brasil",
+                Gender = 'M'
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Abacaxi" + i,
+                LastName = "Abacaxizeiro",
+                Address = "São paulo - Guarulho - Brasil",
+                Gender = 'F'
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
