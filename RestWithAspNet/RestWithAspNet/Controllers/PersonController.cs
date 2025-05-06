8f7cf7d01c00d4151bbb835345829a1079236b4a
t.xavier.ferreira <t.xavier.ferreira@avanade.com>
2025-05-06 10:41:10 -0300

    Ajustado caminho da chamada da API

--- RestWithAspNet/RestWithAspNet/Controllers/PersonController.cs
+++ RestWithAspNet/RestWithAspNet/Controllers/PersonController.cs
@@ -6,7 +6,7 @@ using RestWithAspNet.Services;
 namespace RestWithAspNet.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class PersonController : ControllerBase
     {
         private readonly ILogger<PersonController> _logger;
